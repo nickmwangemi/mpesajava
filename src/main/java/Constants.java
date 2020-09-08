86920392cf49ea5cc72a14027c3ae232d7a2e771
nickmwangemi <nickmwangemi@gmail.com>
2020-09-08 21:09:05 +0300

    Initial commit

---
+++ src/main/java/Constants.java
@@ -0,0 +1,4 @@
+public class Constants {
+    public  static String APP_KEY ="wF1OrBslKxAX9H5WJERAkKzLbjdBHp4H"; //replace with your app api key
+    public static String APP_SECRET="rsYIvxnYIa047egA"; //replace with your app api secret
+}
