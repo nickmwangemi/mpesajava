13c6b51ddee9df9c3a225439aa8b7c5a741bb5d3
nickmwangemi <nickmwangemi@gmail.com>
2020-09-08 21:17:52 +0300

    Updated

--- src/main/java/Constants.java
+++ src/main/java/Constants.java
@@ -1,4 +1,4 @@
 public class Constants {
-    public  static String APP_KEY ="wF1OrBslKxAX9H5WJERAkKzLbjdBHp4H"; //replace with your app api key
-    public static String APP_SECRET="rsYIvxnYIa047egA"; //replace with your app api secret
+    public  static String APP_KEY =""; //replace with your app api key
+    public static String APP_SECRET=""; //replace with your app api secret
 }
